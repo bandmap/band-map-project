4fe594a5082476a124d81634eaa8b683a7a58d1f
sharon12307 <sharon12307@gmail.com>
2024-12-16 00:04:08 +0800

    1215_refresh2

--- src/components/MemberBar.jsx
+++ src/components/MemberBar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function MemberBar() {
     return (
         <div id="member-bar">
-            <Link to='/'><img src="public/images/logo.svg" alt="logo" /></Link>
+            <Link to='/'><img src="/public/images/logo.svg" alt="logo" /></Link>
             <ul className="menu">
                 <li><a href="" className="icon-member"></a></li>
                 <li><a href="" className="icon-msg"></a></li>
